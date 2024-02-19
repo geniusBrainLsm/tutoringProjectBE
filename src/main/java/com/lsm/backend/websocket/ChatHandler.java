0611ad857b150277aca33aaf487a09ae75d161fa
geniusbrainlsm <geniusbrainlsm@gmail.com>
2024-02-19 21:53:58 +0900

    socket authentication error

--- src/main/java/com/lsm/backend/websocket/ChatHandler.java
+++ src/main/java/com/lsm/backend/websocket/ChatHandler.java
@@ -1,4 +1,14 @@
 package com.lsm.backend.websocket;
+import com.lsm.backend.model.User;
+import com.lsm.backend.repository.UserRepository;
+import com.lsm.backend.security.TokenProvider;
+import com.lsm.backend.security.oauth2.user.JwtUserDetails;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.TextMessage;
@@ -8,11 +18,26 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import java.util.HashMap;
 import java.util.Map;
 @Component
+@RequiredArgsConstructor
 public class ChatHandler extends TextWebSocketHandler{
+    private final TokenProvider tokenProvider;
+    private final UserRepository userRepository;
     private Map<String, WebSocketSession> sessions = new HashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        String token = session.getUri().getQuery().split("=")[1];
+        Long userId = tokenProvider.getUserIdFromToken(token);
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("사용자를 찾을 수 없습니다."));
+
+        JwtUserDetails userDetails = new JwtUserDetails(user);
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
         // 소켓 연결시 필요한 작업
         sessions.put(session.getId(), session);
     }
