5ca47ad71a1f28dd2a27ce90b43514b64b29c632
geniusbrainlsm <geniusbrainlsm@gmail.com>
2024-03-24 21:42:17 +0900

    socketfix

--- src/main/java/com/lsm/backend/controller/ChatController.java
+++ src/main/java/com/lsm/backend/controller/ChatController.java
@@ -1,15 +1,15 @@
 package com.lsm.backend.controller;
 
-import com.lsm.backend.model.ChatMessageDTO;
+import com.lsm.backend.payload.ChatMessageDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
+
+import static com.lsm.backend.payload.ChatMessageDTO.MessageType.CHAT;
 
 @RestController
 public class ChatController {
@@ -17,23 +17,22 @@ public class ChatController {
     private SimpMessageSendingOperations messagingTemplate;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    @MessageMapping("/app/chatSendMessage")
-    @SendTo("/topic/public")
-    public ChatMessageDTO sendMessage(@Payload ChatMessageDTO chatMessage) {
+//    @MessageMapping("/app/sendchat")
+//    public void sendMessage(@Payload ChatMessageDTO chatMessageDTO) {
+//        logger.info("Received message from client: " + chatMessageDTO);
+//        messagingTemplate.convertAndSend("/topic/public", chatMessageDTO);
+//    }
+//    @MessageMapping("/app/adduser")
+//    public void addUser(@Payload ChatMessageDTO chatMessageDTO) {
+//        logger.info("joined", chatMessageDTO);
+//        messagingTemplate.convertAndSend("/topic/public", chatMessageDTO); // 메시지를 /topic/public으로 전송
+//    }
 
-        logger.info("Received chat message: {}", chatMessage.getContent());
-        messagingTemplate.convertAndSend(chatMessage);
-        return chatMessage;
+    @MessageMapping("/app/sendchat")
+    public ChatMessageDTO sendChat(@Payload ChatMessageDTO chatMessageDTO) {
+        messagingTemplate.convertAndSend("/topic/public", chatMessageDTO);
+
+        return chatMessageDTO;
     }
-
-    @MessageMapping("/app/chatAddUser")
-    @SendTo("/topic/public")
-    public ChatMessageDTO addUser(@Payload ChatMessageDTO chatMessage) {
-
-        logger.info("joined", chatMessage);
-
-        return chatMessage;
-    }
-
 
 }
--- src/main/java/com/lsm/backend/payload/ChatMessageDTO.java
+++ src/main/java/com/lsm/backend/payload/ChatMessageDTO.java
@@ -1,14 +1,20 @@
-package com.lsm.backend.model;
+package com.lsm.backend.payload;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+
+import java.awt.*;
 
 @Getter
 @Setter
+
 public class ChatMessageDTO {
     private MessageType type;
     private String content;
     private String sender;
+
     public enum MessageType{
         CHAT,
         JOIN,
