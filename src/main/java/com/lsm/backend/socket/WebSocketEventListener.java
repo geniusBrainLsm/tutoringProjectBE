cfc0a1e8e20d91817aed01e1fc5505781d611f0f
geniusbrainlsm <geniusbrainlsm@gmail.com>
2024-03-24 21:42:17 +0900

    socketfix

--- src/main/java/com/lsm/backend/socket/WebSocketEventListener.java
+++ src/main/java/com/lsm/backend/socket/WebSocketEventListener.java
@@ -1,12 +1,11 @@
 package com.lsm.backend.socket;
 
-import com.lsm.backend.model.ChatMessageDTO;
+import com.lsm.backend.payload.ChatMessageDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
-import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
