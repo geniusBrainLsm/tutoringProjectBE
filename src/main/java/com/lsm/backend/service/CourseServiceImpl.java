def00e9c3ce205439a9b325f466f0ca453bddeb4
geniusbrainlsm <geniusbrainlsm@gmail.com>
2024-03-29 19:32:21 +0900

    temporary fix

--- src/main/java/com/lsm/backend/service/CourseServiceImpl.java
+++ src/main/java/com/lsm/backend/service/CourseServiceImpl.java
@@ -1,46 +1,46 @@
-package com.lsm.backend.service;
-
-import com.lsm.backend.model.Course;
-import com.lsm.backend.payload.CourseDTO;
-import com.lsm.backend.repository.CourseRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.stereotype.Service;
-
-import java.awt.print.Pageable;
-import java.util.Optional;
-@Service
-@RequiredArgsConstructor
-public class CourseServiceImpl implements CourseService{
-    private final CourseRepository courseRepository;
-    @Override
-    public CourseDTO createaCourse(CourseDTO courseDTO) {
-        Course course = courseRepository.save(courseDTO.toEntity());
-        return CourseDTO.fromEntity(course);
-    }
-
-    @Override
-    public Optional<CourseDTO> getCourse(Long id) {
-
-    }
-
-    @Override
-    public Page<CourseDTO> getAllCourses(Pageable pageable) {
-
-    }
-
-    @Override
-    public Optional<CourseDTO> getCourse(CourseDTO courseDTO) {
-
-    }
-
-    @Override
-    public void deleteCourse(Long id) {
-
-    }
-
-    @Override
-    public CourseDTO updateCourse(CourseDTO courseDTO) {
-
-    }
-}
+//package com.lsm.backend.service;
+//
+//import com.lsm.backend.model.Course;
+//import com.lsm.backend.payload.CourseDTO;
+//import com.lsm.backend.repository.CourseRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.data.domain.Page;
+//import org.springframework.stereotype.Service;
+//
+//import java.awt.print.Pageable;
+//import java.util.Optional;
+//@Service
+//@RequiredArgsConstructor
+//public class CourseServiceImpl implements CourseService{
+//    private final CourseRepository courseRepository;
+//    @Override
+//    public CourseDTO createaCourse(CourseDTO courseDTO) {
+//        Course course = courseRepository.save(courseDTO.toEntity());
+//        return CourseDTO.fromEntity(course);
+//    }
+//
+//    @Override
+//    public Optional<CourseDTO> getCourse(Long id) {
+//
+//    }
+//
+//    @Override
+//    public Page<CourseDTO> getAllCourses(Pageable pageable) {
+//
+//    }
+//
+//    @Override
+//    public Optional<CourseDTO> getCourse(CourseDTO courseDTO) {
+//
+//    }
+//
+//    @Override
+//    public void deleteCourse(Long id) {
+//
+//    }
+//
+//    @Override
+//    public CourseDTO updateCourse(CourseDTO courseDTO) {
+//
+//    }
+//}
--- src/main/java/com/lsm/backend/service/CurriculumServiceImpl.java
+++ src/main/java/com/lsm/backend/service/CurriculumServiceImpl.java
@@ -1,51 +1,58 @@
-package com.lsm.backend.service;
-
-import com.lsm.backend.model.Course;
-import com.lsm.backend.payload.CurriculumDTO;
-import com.lsm.backend.repository.CourseRepository;
-import com.lsm.backend.repository.CurriculumRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-@RequiredArgsConstructor
-@Service
-public class CurriculumServiceImpl implements CurriculumService{
-    private final CurriculumRepository curriculumRepository;
-    private final CourseRepository courseRepository;
-
-
-    @Override
-    public CurriculumDTO createCurriculum(List<CurriculumDTO> curriculumDTOS ,Long courseId) {
-        Course course = courseRepository.findById(course);
-        List<CurriculumDTO> curriculum = new ArrayList<>();
-
-        for(CurriculumDTO curriculumDTO : ){
-
-        }
-
-        return CurriculumDTO.fromEntity()
-    }
-
-    @Override
-    public CurriculumDTO updateCurriculum(CurriculumDTO curriculumDTO) {
-        return null;
-    }
-
-    @Override
-    public List<CurriculumDTO> getAllCurriculum() {
-        return null;
-    }
-
-    @Override
-    public Optional<CurriculumDTO> getCurriculum(Long id) {
-        return Optional.empty();
-    }
-
-    @Override
-    public void deleteCurriculum(Long id) {
-
-    }
-}
+//package com.lsm.backend.service;
+//
+//import com.lsm.backend.exception.ResourceNotFoundException;
+//import com.lsm.backend.model.Course;
+//import com.lsm.backend.model.Curriculum;
+//import com.lsm.backend.payload.CourseDTO;
+//import com.lsm.backend.payload.CurriculumDTO;
+//import com.lsm.backend.repository.CourseRepository;
+//import com.lsm.backend.repository.CurriculumRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//@RequiredArgsConstructor
+//@Service
+//public class CurriculumServiceImpl implements CurriculumService{
+//    private final CurriculumRepository curriculumRepository;
+//    private final CourseRepository courseRepository;
+//
+//
+//    @Override
+//    public List<Curriculum> createCurriculum(List<CurriculumDTO> curriculumDTOS ,Long courseId) {
+//        Course course = courseRepository.findById(courseId)
+//                .orElseThrow(()->new ResourceNotFoundException("course","id",course.getId()));
+//
+//        List<Curriculum> curricula = new ArrayList<>();
+//
+//        for(CurriculumDTO curriculumDTO : curriculumDTOS){
+//            Curriculum curriculum = new CurriculumDTO().toEntity();
+//            curriculum.setCourse(course);
+//            curricula.add(curriculumRepository.save(curriculum));
+//        }
+//
+//        return curricula;
+//    }
+//
+//    @Override
+//    public CurriculumDTO updateCurriculum(CurriculumDTO curriculumDTO) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<CurriculumDTO> getAllCurriculum() {
+//        return null;
+//    }
+//
+//    @Override
+//    public Optional<CurriculumDTO> getCurriculum(Long id) {
+//        return Optional.empty();
+//    }
+//
+//    @Override
+//    public void deleteCurriculum(Long id) {
+//
+//    }
+//}
