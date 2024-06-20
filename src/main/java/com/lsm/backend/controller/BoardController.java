7e60b9785ee1d08ee3140d3db8072c6edf48fb44
geniusbrainlsm <geniusbrainlsm@gmail.com>
2024-06-20 22:56:49 +0900

    415 err

--- src/main/java/com/lsm/backend/controller/BoardController.java
+++ src/main/java/com/lsm/backend/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,18 +20,22 @@ import java.util.Optional;
 @RequestMapping("/api/board")
 public class BoardController {
     private final BoardServiceImpl boardService;
-    @PostMapping
-    public ResponseEntity<?> createPost(@RequestBody BoardDTO boardDTO, @RequestPart List<MultipartFile> files){
+
+    @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> createPost(@RequestPart("boardDTO") BoardDTO boardDTO,
+                                        @RequestPart("files") List<MultipartFile> files) {
 
         boardDTO.setImage(files);
         BoardDTO createdDTO = boardService.createPost(boardDTO);
         return ResponseEntity.status(201).body(createdDTO);
     }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deletePost(@PathVariable Long id){
         boardService.deletePost(id);
         return ResponseEntity.noContent().build();
     }
+
     @GetMapping("/{id}")
     public ResponseEntity<?> getPost(@PathVariable Long id) {
         Optional<BoardDTO> board = boardService.getPost(id);
@@ -41,22 +46,26 @@ public class BoardController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
     @GetMapping("/search/{boardType}")
-    public ResponseEntity<Page<BoardDTO>> getSearchPost(@PathVariable String boardType ,@RequestParam(required = false) String keyword,
+    public ResponseEntity<Page<BoardDTO>> getSearchPost(@PathVariable String boardType,
+                                                        @RequestParam(required = false) String keyword,
                                                         @RequestParam(required = false) String sortBy,
-                                                        @PageableDefault(page = 1) Pageable pageable){
+                                                        @PageableDefault(page = 1) Pageable pageable) {
 
         Page<BoardDTO> searchResult = boardService.getSearchPost(boardType, keyword, sortBy, pageable);
 
         return ResponseEntity.ok(searchResult);
     }
+
     @GetMapping
-    public ResponseEntity<Page<BoardDTO>> getAllPost(@PageableDefault(page = 1) Pageable pageable){
+    public ResponseEntity<Page<BoardDTO>> getAllPost(@PageableDefault(page = 1) Pageable pageable) {
         Page<BoardDTO> posts = boardService.getAllPost(pageable);
         return ResponseEntity.ok(posts);
     }
+
     @PutMapping("/{id}")
-    public ResponseEntity<?> editPost(@PathVariable Long id, @RequestBody BoardDTO boardDTO){
+    public ResponseEntity<?> editPost(@PathVariable Long id, @RequestBody BoardDTO boardDTO) {
         BoardDTO createdDTO = boardService.updatePost(boardDTO);
         return ResponseEntity.status(201).body(createdDTO);
     }
